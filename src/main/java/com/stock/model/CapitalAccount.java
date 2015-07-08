971b5b301feeda44efc32a4caa55e9fe7f0325fa
kevinge314gh <kevinge314gg@gmail.com>
2015-07-08 09:10:25 +0800

    78

---
+++ src/main/java/com/stock/model/CapitalAccount.java
@@ -0,0 +1,19 @@
+package com.stock.model;
+
+public class CapitalAccount {
+	
+	private int capitalaccountid;
+	private int holderid;
+	private int capitalaccountid;
+	private int capitalaccountid;
+	private int capitalaccountid;
+	private int capitalaccountid;
+	private int capitalaccountid;
+	private int capitalaccountid;
+	private int capitalaccountid;
+	private int capitalaccountid;
+	private int capitalaccountid;
+	private int capitalaccountid;
+	
+
+}
