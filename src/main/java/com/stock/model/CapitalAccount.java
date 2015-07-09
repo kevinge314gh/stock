f3ad3071964bc27c8c6700df76991720a46e56c9
kevinge314gh <kevinge314gg@gmail.com>
2015-07-09 09:03:02 +0800

    modify capitalaccount

--- src/main/java/com/stock/model/CapitalAccount.java
+++ src/main/java/com/stock/model/CapitalAccount.java
@@ -1,19 +1,33 @@
 package com.stock.model;
 
+import java.sql.Date;
+
 public class CapitalAccount {
 	
-	private int capitalaccountid;
+	private int capitala_ccountid;
 	private int holderid;
-	private int capitalaccountid;
-	private int capitalaccountid;
-	private int capitalaccountid;
-	private int capitalaccountid;
-	private int capitalaccountid;
-	private int capitalaccountid;
-	private int capitalaccountid;
-	private int capitalaccountid;
-	private int capitalaccountid;
-	private int capitalaccountid;
+	private Date date;
+	private int d_profitloss_rate;
+	private int d_profitloss;
+	private int yd_assets;
+	private int d_sz_rose;
+	private int w_profitloss_rate;
+	private int w_profitoss;
+	private int w_rank;
+	private int w_operatetimes;
+	private int m_profitloss_rate;
+	private int m_profitloss;
+	private int m_rank;
+	private int m_operatetimes;
+	private int total_profitloss_rate;
+	private int tatal_profitloss;
+	private int float_profitloss;
+	private int realize_profitloss;
+	private int total_assets;
+	private int initial_assets;
+	private int remaining_sum;
+	private int stock_value;
+	
 	
 
 }
